630fc27f78dad4d5ed05be865188d5b6cc266e45
Shadid Chowdhury <shadid.chowdhury@smartbear.com>
2015-09-03 14:03:53 +0200

    Try tutorial methods

--- src/main/java/com/experiments/ResourceMain.java
+++ src/main/java/com/experiments/ResourceMain.java
@@ -21,6 +21,36 @@ public class ResourceMain {
 
     public static void main(String[] args) {
 
+        port(8088);
+
         get("/hello", (request, response) -> "Hello World!");
+
+        post("/hello", (request, response) ->
+                "Hello World: " + request.body()
+        );
+
+        get("/private", (request, response) -> {
+            response.status(401);
+            return "Go Away!!!";
+        });
+
+        get("/users/:name", (request, response) -> "Selected user: " + request.params(":name"));
+
+        get("/news/:section", (request, response) -> {
+            response.type("text/xml");
+            return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><news>" + request.params("section") + "</news>";
+        });
+
+        get("/protected", (request, response) -> {
+            halt(403, "I don't think so!!!");
+            return null;
+        });
+
+        get("/redirect", (request, response) -> {
+            response.redirect("/news/world");
+            return null;
+        });
+
+        get("/", (request, response) -> "root");
     }
 }
